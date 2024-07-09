220d3594959c04f171e7db0a73d8ce3535b37621
Côme Chilliet <come.chilliet@nextcloud.com>
2024-07-09 09:27:10 +0200

    fix(user_ldap): Correctly cache that user exists to avoid a request on mapping new user
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- apps/user_ldap/lib/Access.php
+++ apps/user_ldap/lib/Access.php
@@ -710,6 +710,7 @@ class Access extends LDAPUtility {
 	 */
 	public function cacheUserExists(string $ocName): void {
 		$this->connection->writeToCache('userExists' . $ocName, true);
+		$this->connection->writeToCache('userExistsOnLDAP' . $ocName, true);
 	}
 
 	/**
